a2169a2a934a526aefb95f612863eadcb6515891
Tobias <Tobia@DESKTOP-K02VCHB>
2022-08-23 09:09:37 +0200

    asdad

--- BlockEditor/Settings.Designer.cs
+++ BlockEditor/Settings.Designer.cs
@@ -49,7 +49,7 @@ namespace BlockEditor {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("7")]
         public int Zoom {
             get {
                 return ((int)(this["Zoom"]));
