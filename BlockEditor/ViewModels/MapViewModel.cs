05cf28b5c07b497d276c826e2f02ff32e5d44dfb
Pr2FreeRunner <FreeRunner_pr2_4@mail.com>
2022-09-08 10:25:11 +0200

    asdsa

--- BlockEditor/ViewModels/MapViewModel.cs
+++ BlockEditor/ViewModels/MapViewModel.cs
@@ -26,6 +26,7 @@ namespace BlockEditor.ViewModels
             get { return Game.Map?.Blocks.Overwrite ?? false; }
             set { Game.Map.Blocks.Overwrite = value; RaisePropertyChanged(); }
         }
+
         public Game Game { get; }
         public MenuCommands Commands { get; }
 
